0df587e7f4c28786da5215e995a8fea28fef8b75
Adam Legg <adam.legg@gmail.com>
2016-03-29 19:35:10 -0600

    added mongo and users

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,15 @@ router.get('/', function(req, res) {
 });
 
 
+router.get('/userlist', function(req,res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    })
+  })
+})
+
+
 module.exports = router;
